8405cc047cc89a9f7260cf407a1b23696200b4f0
Tetiana Breslavska <tvk.koba@gmail.com>
2023-03-31 01:55:32 +0200

    Add forms to Table

--- src/components/pages/Table/Table.js
+++ src/components/pages/Table/Table.js
@@ -1,9 +1,13 @@
-// import styles from './Table.module.scss';
+import styles from './Table.module.scss';
 import { useSelector } from 'react-redux';
 import { getOneTable } from '../../../redux/tablesRedux'
 import Button from 'react-bootstrap/Button';
 import SpinnerLine from '../../common/SpinnerLine/SpinnerLine';
 import { useParams } from "react-router";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 
 const Table = props => {
@@ -22,12 +26,44 @@ const Table = props => {
     return (
         <div>
             {tableActiveShow.map(table => 
-                    <div key={table.id}{...table}>
-                        <h1>Table {table.id}</h1>
-                        <p>Status: {table.status} </p>
-                        <p>People:{table.peopleAmount} </p>
-                        <p>Bill: </p>
-                        <Button variant="primary">Update</Button>{' '}
+                    <div className={styles.table_active} key={table.id}{...table}>
+                        <h1 className={styles.title}>Table {table.id}</h1>
+                        <div className={styles.status}>
+                            <span>Status:</span>
+                            <Form.Select value={table.status}>
+                                <option>Free</option>
+                                <option>Reserved</option>
+                                <option>Busy</option>
+                                <option>Cleaning</option>
+                            </Form.Select> 
+                        </div>
+                        <div className={styles.people}>
+                            <span>People:</span>
+                            <Form>
+                                <Row>
+                                    <Col xs={2}>
+                                        <Form.Control value={table.peopleAmount} />
+                                    </Col>
+                                    /
+                                    <Col xs={2}>
+                                        <Form.Control value={table.maxPeopleAmount}/>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
+                        <div className={styles.bill}>
+                            <span>Bill:</span>
+                            <span>$</span>
+                            <Form>
+                                <Row>
+                                    <Col xs={4}>
+                                        <Form.Control value={table.bill} />
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
+
+                        <Button variant="primary">Update</Button>
 
                     </div> 
                     
